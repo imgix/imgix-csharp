57a26cae55183e6eb181e7be228b2bc0cacebee3
Cameron Engh <cengh@cricut.com>
2018-07-19 10:57:43 -0600

    Add Cricut Nuget information

--- Imgix-CSharp/Properties/AssemblyInfo.cs
+++ Imgix-CSharp/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Imgix-CSharp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Cricut fork of the Imgix-CSharp Library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Imgix.com")]
 [assembly: AssemblyProduct("Imgix-CSharp")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
